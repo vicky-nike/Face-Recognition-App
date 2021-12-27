88cdf71de6fb12a223e393277c2699fa747c8bca
vicky-nike <vickyskv14@gmail.com>
2021-12-27 08:24:45 +0530

    real time video feed while recognition and creating dataset in face_app3 and 4

--- FacialRecognition/face_app4.py
+++ FacialRecognition/face_app4.py
@@ -1,24 +1,22 @@
-import threading
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import messagebox
-import matplotlib.pyplot as plt
 import tkinter.font as tkfont
 import cv2
 from time import sleep
+import os
 from imutils.video import WebcamVideoStream
 import imutils
-from threading import Thread
+import pandas as pd
 import csv_add_details
 import face_training
-import os
 import dbus
 
 sess_bus = dbus.SessionBus()
-
 root = Tk()
 root.title('Face detection app')
 root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='face-id.png'))
+
 root.attributes("-fullscreen", True)
 #root.attributes("-zoomed", True)
 #root.geometry("700x400")
@@ -35,21 +33,30 @@ def home():
 
     #font details
     bold_font = tkfont.Font(family="Helvetica", size=12, weight="bold")
+    normal_font = tkfont.Font(family="Helvetica", size=12, slant='italic')
     # labels
     bgTopLabel = Label(root, text="", bg='#dae8fc', font=bold_font, height=3, width=44, borderwidth=1, relief="solid")
     topLabelText = Label(root, text="Face Home", bg='#dae8fc', fg = "black", font=bold_font,justify=LEFT, height=2, width=10)
-    boxLabel = Label(root, text="", bg='#dae8fc',font=bold_font, height=15, width=30)
+    global boxLabelHome
+    boxLabelHome = Label(root, text="", bg='#dae8fc',font=bold_font, height=15, width=30)
     NameLabel = Label(root, text="", font=bold_font,justify=CENTER, height=1, width=10)
     NameEntry = Label(root, text="", font=bold_font,justify=CENTER, height=2, width=15)
-    StartButton = Button(root, text='Start', bg='white', fg='black', font=bold_font, height=1, width=10)
-
+    EnterButton = Label(root, text='', font=bold_font,justify=CENTER, height=2, width=15)
+    msgTitleLabel = Label(root, text="Message box",anchor=CENTER, font=bold_font, height=1, width=8)
+    global msgLabelHome
+    msgLabelHome = Label(root, text=' Click start to begin\nrecognition', font=normal_font, anchor=NW, height=1, width=8, borderwidth=1, relief=SUNKEN)
+    StartButton = Button(root, text='Start', bg='white', fg='black', font=bold_font, height=1, width=10, command=recognition)
+    
     #place labels
     bgTopLabel.place(anchor=W, relheight=0.15, relwidth=0.686, relx=0.295, rely=0.1)
     topLabelText.place(anchor=W, relheight=0.1, relwidth=0.2, relx=0.31, rely=0.1)
-    boxLabel.place(anchor=W, relheight=0.75, relwidth=0.46, relx=0.295, rely=0.6)
+    boxLabelHome.place(anchor=W, relheight=0.75, relwidth=0.46, relx=0.295, rely=0.6)
     NameLabel.place(anchor=W, relheight=0.1, relwidth=0.2, relx=0.77, rely=0.26)
     NameEntry.place(anchor=W, relheight=0.1, relwidth=0.25, relx=0.77, rely=0.37)
+    EnterButton.place(anchor = W, relheight=0.06, relwidth=0.215, relx=0.77, rely=0.45)
     StartButton.place(anchor=W, relheight=0.1, relwidth=0.215, relx=0.77, rely=0.92)
+    msgTitleLabel.place(anchor=W, relheight=0.1, relwidth=0.2, relx= 0.77, rely=0.6)
+    msgLabelHome.place(anchor=W, relheight=0.1, relwidth=0.2, relx=0.77, rely=0.7)
 
 def enrollNew():
 
@@ -74,8 +81,8 @@ def enrollNew():
     NameEntry.bind('<FocusIn>', keyboardShow)
     EnterButton = Button(root, text='Enter', bg='white', fg='black', font=bold_font, height=1, width=10, command=lambda: [rootFocus(), keyboardHide()])
     msgTitleLabel = Label(root, text="Message box",anchor=CENTER, font=bold_font, height=1, width=8)
-    global msgLabel
-    msgLabel = Label(root, text=' Enter name and click start', font=normal_font, anchor=NW, height=1, width=8, borderwidth=1, relief=SUNKEN)
+    global msgLabelNew
+    msgLabelNew = Label(root, text=' Enter name and click start', font=normal_font, anchor=NW, height=1, width=8, borderwidth=1, relief=SUNKEN)
     global StartButton
     StartButton = Button(root, text='Start', bg='white', fg='black', font=bold_font, height=1, width=10, command=getName)
 
@@ -87,7 +94,7 @@ def enrollNew():
     NameEntry.place(anchor=W, relheight=0.06, relwidth=0.215, relx=0.77, rely=0.37)
     EnterButton.place(anchor = W, relheight=0.06, relwidth=0.215, relx=0.77, rely=0.45)
     msgTitleLabel.place(anchor=W, relheight=0.1, relwidth=0.2, relx= 0.77, rely=0.6)
-    msgLabel.place(anchor=W, relheight=0.1, relwidth=0.2, relx=0.77, rely=0.7)
+    msgLabelNew.place(anchor=W, relheight=0.1, relwidth=0.2, relx=0.77, rely=0.7)
     StartButton.place(anchor=W, relheight=0.1, relwidth=0.215, relx=0.77, rely=0.92)
 
 def editDatabase():
@@ -137,9 +144,9 @@ def getName():
 
 def enrollNewStart(name):
     print(name)
-    global id, homeButton, enrollNewButton, editDatabaseButton, settingsButton
+    global id, homeButton, enrollNewButton, editDatabaseButton, settingsButton, msgLabelNew
     id = int(csv_add_details.create(name))
-    msgLabel['text'] = "    Name got registered "
+    msgLabelNew['text'] = "    Name got registered "
     StartButton['state'] = DISABLED
     homeButton['state'] = DISABLED
     enrollNewButton['state'] = DISABLED
@@ -149,122 +156,143 @@ def enrollNewStart(name):
     makeDataset()
 
 def makeDataset():
+    global msgLabelNew
     print("preparing dataset")
     messagebox.showinfo("Attention", "Initializing face capture. Look at the camera and wait ...")
-    msgLabel['text'] = "  Preparing dataset"
+    msgLabelNew['text'] = "  Preparing dataset"
     global id
-    dataset(id, 1, 0)
-    msgLabel['text'] = "  Preparing dataset\n>> >>"
-    messagebox.showinfo("Attention","Now turn your head 30 degrees left")
-    dataset(id, 2, 10)
-    msgLabel['text'] = "  Preparing dataset\n>> >> >> >>"
-    messagebox.showinfo("Attention","Now turn your head 30 degrees right")
-    dataset(id, 3, 20)
-    msgLabel['text'] = "  Preparing dataset completed"
-    face_training.trainStart()
-    msgLabel['text'] = "Your face details got saved\nand trained "
-    StartButton['state'] = NORMAL
-    homeButton['state'] = NORMAL
-    enrollNewButton['state'] = NORMAL
-    editDatabaseButton['state'] = NORMAL
-    settingsButton['state'] = NORMAL
-
-def dataset(face_id, part, count):
-    global boxLabel
     vs = WebcamVideoStream(src=0).start()
+    #cam = cv2.VideoCapture(0)
+    #cam.set(3, 640) # set video widht
+    #cam.set(4, 480) # set video height
     #face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    
-    while(True):
+    count = 0
 
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
-        #frame1 = imutils.resize(frame, width = 700)
+    def dataset(face_id, count):
+        global boxLabel
+        global msgLabelNew
+
+        #frame= cv2.cvtColor(cam.read()[1],cv2.COLOR_BGR2RGB)
+        frame= cv2.cvtColor(vs.read(),cv2.COLOR_BGR2RGB)
         frame = cv2.flip(frame, -1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Get the latest frame and convert into Image
-        cv2image= cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        dis_img = Image.fromarray(cv2image)
+        img1 = Image.fromarray(frame)
         # Convert image to PhotoImage
-        imgtk = ImageTk.PhotoImage(image = dis_img)
+        imgtk = ImageTk.PhotoImage(image = img1)
         boxLabel.imgtk = imgtk
         boxLabel.configure(image=imgtk)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+        #sleep(0.5)
 
         for (x,y,w,h) in faces: 
 
             cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)     
             # Save the captured image into the datasets folder
+            msgLabelNew['text'] = count+1
             count += 1
-            print(count)
             cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
 
-        if count == 10 and part == 1: # Take 10 face sample and stop video
-            break
-        elif count == 20 and part == 2:
-            break
+        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        
+        if count == 10: # Take 10 face sample and stop video
+            messagebox.showinfo("Attention","Now turn your head 30 degrees left")
+            count += 1
+            boxLabel.after(20, lambda: dataset(face_id, count))
+        elif count == 20:
+            messagebox.showinfo("Attention","Now turn your head 30 degrees right")
+            count += 1
+            boxLabel.after(20, lambda: dataset(face_id, count))
         elif count >= 30:
-            break
-
-    # Do a bit of cleanup
-    print("\n [INFO] Exiting Program and cleanup stuff")
-    cv2.destroyAllWindows()
-    vs.stop()
-
-def dataset1(face_id, part, count):
-    cam = cv2.VideoCapture(0)
-    cam.set(3, 640) # set video width
-    cam.set(4, 480) # set video height
-    #face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-
-    while(True):
-
-        ret, img = cam.read()
-        img1 = cv2.flip(img, -1)
-        gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-
-        faces = face_detector.detectMultiScale(gray)
-
-        for (x, y, w, h) in faces: 
-
-            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
-            # Save the captured image into the datasets folder
-            count += 1
-            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
-            cv2.imshow('camera', img)
-            print(count)
+            #msgLabelNew['text'] = "Preparing dataset completed"
+            face_training.trainStart()
+            msgLabelNew['text'] = "Your face details got saved\nand trained "
+            StartButton['state'] = NORMAL
+            homeButton['state'] = NORMAL
+            enrollNewButton['state'] = NORMAL
+            editDatabaseButton['state'] = NORMAL
+            settingsButton['state'] = NORMAL
+            return
+        else:
+            boxLabel.after(20, lambda: dataset(face_id, count))
     
-        if count == 10 and part == 1: # Take 10 face sample and stop video
-            break
-        elif count == 20 and part == 2:
-            break
-        elif count >= 30:
+    dataset(id, count)
+
+def recognition():
+    global msgLabelHome
+    global boxLabelHome
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read('trainer/trainer.yml')
+    cascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    #iniciate id counter
+    count=1
+    # names related to ids: example ==> Marcelo: id=1,  etc
+    data = pd.read_csv("records.csv")
+    names = data['Name'].tolist()
+
+    # Initialize and start realtime video capture
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640) # set video widht
+    cam.set(4, 480) # set video height
+
+    # Define min window size to be recognized as a face
+    minW = 0.1*cam.get(3)
+    minH = 0.1*cam.get(4)
+    img= cv2.cvtColor(cam.read()[1],cv2.COLOR_BGR2RGB)
+    img = cv2.flip(img, -1)
+    img1 = Image.fromarray(img)
+    # Convert image to PhotoImage
+    imgtk = ImageTk.PhotoImage(image = img1)
+    boxLabelHome.imgtk = imgtk
+    boxLabelHome.configure(image=imgtk)
+    #img = cv2.flip(img, -1) # Flip vertically
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+    )
+    conf = 0
+    for(x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        conf = round(100 - confidence)
+        print("id", id, " conf", conf)
+        found = str(names[id])
+        if(conf>20):
+            msgLabelHome['text'] = "Recognised as " + found
             break
 
-    # Do a bit of cleanup
-    print("\n [INFO] Exiting Program and cleanup stuff")
-    cam.release()
-    cv2.destroyAllWindows()
+    
+    #cv2.imshow('camera',img)
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if(conf>20):
+        return
+    if(count==1):
+        boxLabelHome.after(20, recognition)
 
 def keyboardShow(event):
-    os.system("florence -f show")
+    kbd.Show()
 
 def keyboardHide():
-    os.system("florence -f hide")
+    kbd.Hide()
 
 def rootFocus():
     root.focus()
 
-def stay_at_bottom():
-   root.lower()
-   root.after(1, stay_at_bottom)
+#Start ONboard
+kbd = sess_bus.get_object('org.onboard.Onboard', '/org/onboard/Onboard/Keyboard')
+#root.attributes("-fullscreen", True)
+#root.bind_class("Entry", "<1>", lambda ev: ev.widget.focus_force())
 
-#Start florence keyboard
-#kbd = sess_bus.get_object('org.florence.Keyboard', '/org/florence/Keyboard')
-stay_at_bottom()
 #BG labels
 labelLeft = Label(root, text="", bg="#6963ff", height= 60, width=25)
 image = Image.open('logo-main.png')
